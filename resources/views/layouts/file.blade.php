0afc5bcb7223230646045d2d27ae9eccd6e62e83
KhushpreetSingh846 <khush.presolutionslab@gmail.com>
2021-05-26 21:44:29 +0530

    file

---
+++ resources/views/layouts/file.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Menu </title>
+        <link rel="stylesheet" href="{{ URL::asset('css/menu.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    </head>
+    <body>
+    </body>
+</html>
